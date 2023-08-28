6a52e7189f1028fca892efca3ff4d74ecb826b52
Jules Clerc <jules.clerc@epitech.eu>
2023-08-28 16:25:40 +0200

    portfolio release

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,11 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
           rel="stylesheet"
         />
+          <link rel="shortcut icon" href="/icons8-portefeuille-16.png" />
+          <link rel="apple-touch-icon" sizes="96x96" href="/icons8-portefeuille-96.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons8-portefeuille-32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons8-portefeuille-16.png" />
+          <title>Jules Clerc Portfolio</title>
       </Head>
       <body className="font-poppins">
         <Main />
